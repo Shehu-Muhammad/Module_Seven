0a6b9ac923f11c797b9b6d45b589c84bc817b305
Student <student@StudentiMac-33.local>
2022-08-19 14:17:04 -0400

    [DOCS] added template for website mockup

---
+++ index.py
@@ -0,0 +1,25 @@
+def my_function():
+    thisTuple = ["Hello", "World", "Apples"]
+    for x in thisTuple:
+        print(x)
+
+# my_function()
+
+def writeNameToPage():
+    username = input("Enter a username: ")
+    file = open("test.txt", 'a')
+    file.write(username)
+    file.close()
+# writeNameToPage()
+
+def readNameFromPage():
+    file = open("test.txt", "r")
+    print(file.read())
+
+# readNameFromPage()
+
+
+
+# print(username)
+
+
